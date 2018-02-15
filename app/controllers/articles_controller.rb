5a47c0107f0f99dd31b9a64d5f8f0c38ba71ae5d
Raymond Seger <raymondseger@live.com>
2018-02-15 22:12:51 +0700

    create and show methods and the template
    create and show methods and the template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,14 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    @article.save
+    redirect_to @article
   end
 end
